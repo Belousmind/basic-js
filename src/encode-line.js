3571c6ab3ee39bfdd328a58fbd17bec16e45573b
Darya Belousova <belousmind@gmail.com>
2023-12-18 15:11:10 +0100

    solve 'sumOfCode'

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,6 +10,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
+
+// сначала ищем униканльые значения, далее считаем их и потом пищем кол-во и значение
+
 function encodeLine(/* str */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
